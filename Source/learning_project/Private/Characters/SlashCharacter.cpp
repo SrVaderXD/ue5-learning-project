05daec44539ef00fc9a25ee85a42d8fbcd9c78ed
SrVaderXD <62197288+SrVaderXD@users.noreply.github.com>
2026-01-14 12:50:56 -0300

    feat: add enhanced input to character
    Added movement and mouse look input bindings to the character C++ class

--- Source/learning_project/Private/Characters/SlashCharacter.cpp
+++ Source/learning_project/Private/Characters/SlashCharacter.cpp
@@ -1,5 +1,9 @@
 #include "Characters/SlashCharacter.h"
 
+#include "Components/InputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
 ASlashCharacter::ASlashCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
@@ -9,7 +13,36 @@ ASlashCharacter::ASlashCharacter()
 void ASlashCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(SlashMappingContext, 0);
+		}
+	}
 	
+}
+
+void ASlashCharacter::Move(const FInputActionValue& Value)
+{
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	const FVector Forward = GetActorForwardVector();
+	AddMovementInput(Forward, MovementVector.Y);
+
+	const FVector Right = GetActorRightVector();
+	AddMovementInput(Right, MovementVector.X);
+}
+
+void ASlashCharacter::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisValue = Value.Get<FVector2D>();
+	if (GetController())
+	{
+		AddControllerYawInput(LookAxisValue.X);
+		AddControllerPitchInput(LookAxisValue.Y);
+	}
 }
 
 void ASlashCharacter::Tick(float DeltaTime)
@@ -21,6 +54,12 @@ void ASlashCharacter::Tick(float DeltaTime)
 void ASlashCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(SlashMoveAction, ETriggerEvent::Triggered, this, &ASlashCharacter::Move);
+		EnhancedInputComponent->BindAction(SlashLookAction, ETriggerEvent::Triggered, this, &ASlashCharacter::Look);
+	}
 
 }
 
--- Source/learning_project/Public/Characters/SlashCharacter.h
+++ Source/learning_project/Public/Characters/SlashCharacter.h
@@ -2,7 +2,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "SlashCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class LEARNING_PROJECT_API ASlashCharacter : public ACharacter
@@ -14,6 +18,19 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputMappingContext* SlashMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* SlashMoveAction;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* SlashLookAction;
+
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+
 
 public:	
 	virtual void Tick(float DeltaTime) override;
