55d3a199ea59a664df6ed624847ce90d357c227b
SrVaderXD <62197288+SrVaderXD@users.noreply.github.com>
2026-01-29 19:48:26 -0300

    feat: detect overlap with Items
    Added a collision component (sphere) and bound OnComponentBeginOverlap to detect and log overlapping actors

--- Source/learning_project/Private/Items/Items.cpp
+++ Source/learning_project/Private/Items/Items.cpp
@@ -1,5 +1,6 @@
 #include "Items/Items.h"
 #include "learning_project/DebugMacros.h"
+#include "Components/SphereComponent.h"
 
 AItems::AItems()
 {
@@ -7,11 +8,16 @@ AItems::AItems()
 
 	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
 	RootComponent = ItemMesh;
+
+	Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere"));
+	Sphere->SetupAttachment(GetRootComponent());
 }
 
 void AItems::BeginPlay()
 {
 	Super::BeginPlay();
+
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AItems::OnSphereOverlap);
 }
 
 float AItems::TransformedSin()
@@ -24,6 +30,15 @@ float AItems::TransformedCos()
 	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
 }
 
+void AItems::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	const FString OtherActorName = OtherActor->GetName();
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Blue, OtherActorName);
+	}
+}
+
 void AItems::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
@@ -34,12 +49,12 @@ void AItems::Tick(float DeltaTime)
 
 	//AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));
 
-	AddActorWorldRotation(FRotator(RotationPitch * DeltaTime, RotationYaw * DeltaTime, RotationRoll * DeltaTime));
+	//AddActorWorldRotation(FRotator(RotationPitch * DeltaTime, RotationYaw * DeltaTime, RotationRoll * DeltaTime));
 
-	DRAW_SPHERE_SingleFrame(GetActorLocation());
-	DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
+	//DRAW_SPHERE_SingleFrame(GetActorLocation());
+	//DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
 
-	FVector AvgVector = Avg<FVector>(GetActorLocation(), FVector::ZeroVector);
-	DRAW_POINT_SingleFrame(AvgVector);
+	//FVector AvgVector = Avg<FVector>(GetActorLocation(), FVector::ZeroVector);
+	//DRAW_POINT_SingleFrame(AvgVector);
 }
 
