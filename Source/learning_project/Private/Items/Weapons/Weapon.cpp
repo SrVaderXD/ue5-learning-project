ce3661bfb32ebff0aec6d24b74af962fe0f3babe
SrVaderXD <62197288+SrVaderXD@users.noreply.github.com>
2026-03-10 22:21:47 -0300

    feat: add sound effects to sword draw, sheath, and equip
    Also fixes weapon hitbox collision remaining active while equipped.

--- Source/learning_project/Private/Items/Weapons/Weapon.cpp
+++ Source/learning_project/Private/Items/Weapons/Weapon.cpp
@@ -1,10 +1,21 @@
 #include "Items/Weapons/Weapon.h"
 #include "Characters/SlashCharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/SphereComponent.h"
 
 void AWeapon::Equip(USceneComponent* Parent, FName SocketName)
 {
 	AttachMeshToSocket(Parent, SocketName);
 	ItemState = EItemState::EIS_Equipped;
+	if (EquipSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this,EquipSound,GetActorLocation());
+	}
+
+	if (Sphere)
+	{
+		Sphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	}
 }
 
 void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
--- Source/learning_project/Public/Items/Weapons/Weapon.h
+++ Source/learning_project/Public/Items/Weapons/Weapon.h
@@ -4,6 +4,7 @@
 #include "Items/Items.h"
 #include "Weapon.generated.h"
 
+class USoundBase;
 
 UCLASS()
 class LEARNING_PROJECT_API AWeapon : public AItems
@@ -17,4 +18,8 @@ public:
 protected:
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 	virtual void OnSphereOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Weapon Properties")
+	USoundBase* EquipSound;
 };
