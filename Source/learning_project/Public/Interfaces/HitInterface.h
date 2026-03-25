24d61a3f5ef114fdd250b053ed06c99880e4cb4a
SrVaderXD <62197288+SrVaderXD@users.noreply.github.com>
2026-03-25 19:24:56 -0300

    feat: add hit detection interface
    adds an Unreal Interface to detect when an Entity collides with the sword

---
+++ Source/learning_project/Public/Interfaces/HitInterface.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "HitInterface.generated.h"
+
+UINTERFACE(MinimalAPI)
+class UHitInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class LEARNING_PROJECT_API IHitInterface
+{
+	GENERATED_BODY()
+
+public:
+};
