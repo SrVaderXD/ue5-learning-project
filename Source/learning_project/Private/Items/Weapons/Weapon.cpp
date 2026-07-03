3a36c4208b8e48201d1356744a6d8b487b6cf774
SrVaderXD <62197288+SrVaderXD@users.noreply.github.com>
2026-07-03 20:25:35 -0300

    feat: add force field to the weapon
    On hit a force field is created to break an object

--- Source/learning_project/Private/Items/Weapons/Weapon.cpp
+++ Source/learning_project/Private/Items/Weapons/Weapon.cpp
@@ -98,5 +98,7 @@ void AWeapon::OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* Oth
 			HitInterface->GetHit(BoxHit.ImpactPoint);
 		}
 		IgnoreActors.AddUnique(BoxHit.GetActor());
+
+		CreateFields(BoxHit.ImpactPoint);
 	}
 }
